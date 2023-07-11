520f7de2967f0ab5a1282d7577f77405d08ed2c2
Dustin Dortch <me@dustindortch.com>
2023-07-11 13:50:19 -0400

    First commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  cloud {
+    organization = "DustinDortch"
+
+    workspaces {
+      name = "workspace-name"
+    }
+  }
+}
+
+variable "first_value" {
+  default = 5
+  type    = number
+}
+
+variable "second_value" {
+  default = 3
+  type    = number
+}
+
+locals {
+  sum = var.first_value + var.second_value
+}
+
+output "test_me" {
+  value = local.sum
+}
